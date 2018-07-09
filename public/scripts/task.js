223551c6d1ab8d62c7c8283d1945056dfe9a7f6a
samburkeCS <sam.fitz.burke@gmail.com>
2018-07-10 08:51:33 +1000

    Fixed adding new tasks

--- public/scripts/task.js
+++ public/scripts/task.js
@@ -106,7 +106,7 @@ function addTaskCard(task)
 
 	var html = "";
 	html += `<div class="col-sm-4">
-				<div class="col-sm-12 bg-light task-card p-3 mb-5 rounded" data-toggle="modal" data-target="#task735595587499755">
+				<div class="col-sm-12 bg-light task-card p-3 mb-5 rounded" data-toggle="modal" data-target="#task${task.id}">
 					<div class="row">
 						<div class="col-sm-12">
 							<h5 class="text-center task-header">${task.name}</h5>
@@ -126,27 +126,27 @@ function addTaskCard(task)
 				<div class="col-12">
 					<div class="modal fade" id="task${task.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" style="display: none;" aria-hidden="true">
 						<div class="modal-dialog" role="document">
-							<div class="modal-content">
+							<div class="loader"></div><div class="modal-content">
 								<div class="modal-header">
 									<h5 class="modal-title" id="exampleModalLabel">${task.name}</h5>
 									<button class="close" type="button" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true">×</span></button>
+										<span aria-hidden="true">×</span>
+									</button>
 								</div>
 								<div class="modal-body">
+									<div class="task-details">
+										<p>${task.notes}</p>
+									</div>
 									<form>
 										<div class="form-group">
-											<label class="col-form-label" for="name">Task name:</label>
-											<input class="form-control" type="text" id="name" name="name" value="${task.name}">
-										</div>
-										<div class="form-group">
-											<label class="col-form-label" for="notes">Task description:</label>
-											<textarea class="form-control" id="notes" name="notes" rows="15">${task.notes}</textarea>
+											<label class="col-form-label" for="comment">New comment:</label>
+											<textarea class="form-control" id="comment" name="comment" rows="5"></textarea>
 										</div>
 									</form>
 								</div>
 								<div class="modal-footer">
 									<button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
-									<button class="btn btn-primary" type="button" data-dismiss="modal" onclick="updateTask(${task.id})">Update </button>
+									<button class="btn btn-primary" type="button" onclick="addComment('${task.id}')">Comment </button>
 								</div>
 							</div>
 						</div>
